a5638445c4441ca5a0c65c126c2e44aecba61ef6
Guillaume Cleroux <cleg1805@usherbrooke.ca>
2022-06-28 20:16:22 -0400

    More game features added, still not done

--- src/Game/tetris_game.jl
+++ src/Game/tetris_game.jl
@@ -10,10 +10,10 @@ Base.@kwdef mutable struct TetrisGame{T<:Integer} <: AbstractGame
     level::T = 1
     line_count::T = 0
     score::T = 0
-    active_piece::AbstractTetromino = get_random_piece()
-    hold_piece::Union{AbstractTetromino, Nothing} = nothing
     bag::Bag = Bag()
-    grid::Grid = Grid()
+    active_piece::AbstractTetromino = pop_piece!(bag)
+    hold_piece::Union{AbstractTetromino, Nothing} = nothing
+    grid::Grid = put_piece!(active_piece, Grid())
 end
 
 function rotate_left!(game::AbstractGame)
@@ -23,7 +23,7 @@ function rotate_left!(game::AbstractGame)
     rotate_left!(game.active_piece)
 
     # Place the piece on the grid
-    put_piece!(game)
+    put_piece!(game.active_piece, game.grid)
 
     return
 end
@@ -37,36 +37,60 @@ function rotate_right!(game::AbstractGame)
     rotate_right!(game.active_piece)
 
     # Place the piece on the grid
-    put_piece!(game)
+    put_piece!(game.active_piece, game.grid)
 
     return
 end
 
+"""
+Clear the space occupied by the active piece in the grid
+
+If the active piece overlap with other pieces in the grid, the other pieces will
+not be cleared.
+"""
 function clear_piece_cells!(game::AbstractGame)
-    let p = game.active_piece,
-            x = p.x,
-            y = p.y,
-            nb_rows = size(p.shapes[p.idx], 1),
-            nb_cols = size(p.shapes[p.idx], 2)
-
-            # Clear the board at the current position of the piece
-            game.grid.cells[x:x+nb_rows, y:y+nb_cols] .= 0
-    end
-    return
-end
-
-function put_piece!(game::AbstractGame)
     let p = game.active_piece,
         x = p.x,
         y = p.y,
-        nb_rows = size(p.shapes[p.idx], 1), 
-        nb_cols = size(p.shapes[p.idx], 2)
+        nb_rows = size(p.shapes[p.idx], 1),
+        nb_cols = size(p.shapes[p.idx], 2),
+        board = game.grid.cells
 
-        # Place the piece on the grid
-        for i in 1:nb_rows, j in 1:nb_cols
-            game.grid.cells[x+i-1, y+j-1] = p.shapes[p.idx][i, j]
+        # Clear the board at the current position of the piece
+        [board[x, y] = 0 for x in x:x+nb_rows, y in y:y+nb_cols if board[x, y] == p.id]
+    end
+    return
+end
+
+
+function check_for_lines!(game::AbstractGame)
+    let NB_VISIBLE_ROWS = 20,
+        NB_HIDDEN_ROWS = game.grid.rows - NB_VISIBLE_ROWS,
+        SCORE_LIST = [100, 300, 500, 800]   # Scores from single to tetris
+
+        local cleared_lines = 0
+
+        # We ignore the hidden rows when looking for lines
+        for row in NB_HIDDEN_ROWS+1:game.grid.rows
+            local is_full = true
+            for col in 1:game.grid.cols
+                if game.grid.cells[row, col] == 0
+                    is_full = false
+                    break
+                end
+            end
+
+            # Every block in the row is occupied, whe delete
+            if is_full
+                game.grid.cells[row, :] .= 0
+                cleared_lines += 1
+                downshift!(game.grid, row)
+            end
+        end
+
+        if cleared_lines != 0
+            game.score += SCORE_LIST[cleared_lines] * game.level
         end
     end
-
     return
 end
