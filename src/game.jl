0894573317661c0293fd76b45a030daf8b8f4807
Guillaume Cleroux <cleg1805@usherbrooke.ca>
2022-06-29 20:44:28 -0400

    Integrated tetrisGame object into ui game

--- src/game.jl
+++ src/game.jl
@@ -1,11 +1,13 @@
 #init 
+using TetrisAI
+
+global game = TetrisGame()
 
 WIDTH = 1000
 HEIGHT = 1000
 
 Paused = false
 
-Score = 0
 Lines = 0
 Timer = 0
 Level = 0
@@ -24,37 +26,9 @@ T = Actor("t.png")
 Z = Actor("z.png")
 pause = Actor("pause.png")
 
-#scenrio for testing
-board = fill(0,10,20)
-board[1,20] = 1
-board[1,19] = 1
-board[1,18] = 1
-board[1,17] = 1
-board[2,20] = 6
-board[3,20] = 6
-board[4,20] = 6
-board[3,19] = 6
-
 gravityDict = Dict([(1,43), (2,38), (3,33), (4,28), (5,23), (6,18), (7,13), (8,8), (9,6), (10,5), (13,4), (16,3), (19,2), (29,1)])
 
 tetrominoesDict = Dict([(0, 0), (1, I), (2, J), (3, L), (4, O), (5, S), (6, T), (7, Z)])
-
-# Commands
-function moveLeft()
-
-end
-
-function moveRight()
-
-end
-
-function rotateClockwise()
-
-end
-
-function rotateCounterclockwise()
-
-end
 
 function softDrop()
     resetTimer()
@@ -78,13 +52,19 @@ end
 
 # Functions
 
-function drawBoard()
-    for i in 1:10, j in 1:20
-        value = board[i,j]
-        if value > 0
-            square = tetrominoesDict[value]
-            square.center = (40i + 30, 40j + 80)
-            draw(square)
+function drawBoard(grid::TetrisAI.Game.AbstractGrid)
+
+    let NB_ROWS = game.grid.rows,
+        NB_VISIBLE_ROWS = 20,
+        NB_HIDDEN_ROWS = NB_ROWS - NB_VISIBLE_ROWS
+
+        for i in NB_HIDDEN_ROWS+1:NB_ROWS, j in 1:grid.cols
+            value = grid.cells[i,j]
+            if value > 0
+                square = tetrominoesDict[value]
+                square.center = (40j + 30, 40i - 40)
+                draw(square)
+            end
         end
     end
 end
@@ -92,20 +72,24 @@ end
 function on_key_down(g::Game, k)
     if !Paused
         if (k == Keys.LEFT)
-            moveLeft()
-            levelUp()
+            TetrisAI.Game.move_left!(game)
         elseif (k == Keys.RIGHT)
-            moveRight()
+            TetrisAI.Game.move_right!(game)
         elseif (k == Keys.UP || k == Keys.X)
-            rotateClockwise()
+            TetrisAI.Tetrominoes.rotate_right!(game)
         elseif (k == Keys.LCTRL || k == Keys.Z)
-            rotateCounterclockwise()
+            TetrisAI.Tetrominoes.rotate_left!(game)
         elseif (k == Keys.DOWN)
             softDrop()
         elseif (k == Keys.SPACE)
-            hardDrop()
+            TetrisAI.Game.hard_drop_piece!(game)
+            TetrisAI.Game.play_step!(game)
+            resetTimer()
         elseif (k == Keys.LSHIFT || k == Keys.C)
             holdPiece()
+        elseif (k == Keys.D)
+            # Debug print
+            println(game)
         end
     end
     if k == Keys.P
@@ -118,6 +102,8 @@ end
 function timeStep()
     global Timer += 1
     if Timer >= Gravity
+        print(game)
+        TetrisAI.Game.play_step!(game)
         resetTimer()
     end
 end
@@ -135,14 +121,14 @@ end
 
 function draw(g::Game)
     draw(bg)
-    drawBoard()
+    drawBoard(game.grid)
     txt = TextActor(string(Timer), "bold"; font_size = 50, color = Int[255, 255, 255, 255])
     txt.pos = (0,0)
     draw(txt)
     lvl = TextActor(string(Level), "bold"; font_size = 50, color = Int[255, 255, 255, 255])
     lvl.center = (875,425)
     draw(lvl)
-    scr = TextActor(string(Score), "bold"; font_size = 50, color = Int[255, 255, 255, 255])
+    scr = TextActor(string(game.score), "bold"; font_size = 50, color = Int[255, 255, 255, 255])
     scr.center = (750,625)
     draw(scr)
     lnz = TextActor(string(Lines), "bold"; font_size = 50, color = Int[255, 255, 255, 255])
