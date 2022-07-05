2b0eb5b5a98a0a9a40d2d5c9129e4383e7b51d19
chal2525 <leo.chartrand@usherbrooke.ca>
2022-07-05 17:22:12 -0500

    Gameover screen, display next pieces and held piece

--- src/game.jl
+++ src/game.jl
@@ -6,6 +6,7 @@ global game = TetrisGame()
 # Sprites
 bg = Actor("bg.png")
 pause = Actor("pause.png")
+gameover = Actor("gameover.png")
 I = Actor("i.png")
 J = Actor("j.png")
 L = Actor("l.png")
@@ -13,6 +14,13 @@ O = Actor("o.png")
 S = Actor("s.png")
 T = Actor("t.png")
 Z = Actor("z.png")
+I_preview = Actor("i_preview.png")
+J_preview = Actor("j_preview.png")
+L_preview = Actor("l_preview.png")
+O_preview = Actor("o_preview.png")
+S_preview = Actor("s_preview.png")
+T_preview = Actor("t_preview.png")
+Z_preview = Actor("z_preview.png")
 
 WIDTH = 1000
 HEIGHT = 1000
@@ -20,10 +28,13 @@ HEIGHT = 1000
 # Dict should be changed to map all levels or change the Gravity adjustments in timestep
 gravityDict = Dict([(0, 48), (1,43), (2,38), (3,33), (4,28), (5,23), (6,18), (7,13), (8,8), (9,6), (10,5), (13,4), (16,3), (19,2), (29,1)])
 tetrominoesDict = Dict([(0, 0), (1, I), (2, J), (3, L), (4, O), (5, S), (6, T), (7, Z)])
+previewsDict = Dict([(0, 0), (1, I_preview), (2, J_preview), (3, L_preview), (4, O_preview), (5, S_preview), (6, T_preview), (7, Z_preview)])
 
+"""
+Sets gravity according to the game level.
+"""
 function levelUp()
     global Gravity
-
     let level = game.level
         while !(level in keys(gravityDict))
             level -= 1
@@ -36,19 +47,27 @@ Paused = false
 Timer = 0
 Gravity = levelUp()
 
+"""
+Pauses or unpauses the game.
+"""
 function pauseGame()
     global Paused = !Paused
 end
 
+"""
+Quits the game.
+"""
 function quitGame()
     exit()
 end
 
-# Functions
-
-function drawBoard(grid::TetrisAI.Game.AbstractGrid)
-
-    let NB_ROWS = grid.rows,
+"""
+Draws the game board on screen.
+"""
+function drawBoard()
+    global game
+    let grid = game.grid,
+        NB_ROWS = grid.rows,
         NB_COLS = grid.cols,
         NB_VISIBLE_ROWS = 20,
         NB_HIDDEN_ROWS = NB_ROWS - NB_VISIBLE_ROWS
@@ -65,8 +84,45 @@ function drawBoard(grid::TetrisAI.Game.AbstractGrid)
     return
 end
 
+"""
+Draws the 3 next pieces.
+"""
+function drawNextPieces()
+    global game
+    x = 625
+    preview_pieces = get_preview_pieces(game.bag)
+    for piece in preview_pieces
+        preview = previewsDict[piece.color]
+        preview.center = (x,225)
+        draw(preview)
+        x += 130
+    end
+    return
+end
+
+"""
+Draws the held piece.
+"""
+function drawHeldPiece()
+    global game
+    if !(game.hold_piece === nothing)
+        piece = previewsDict[game.hold_piece.color]
+        piece.center = (625, 425)
+        draw(piece)
+    end
+end
+
+"""
+Checks for keyboard input.
+"""
 function on_key_down(g::Game, k)
     global game
+    if game.is_over
+        if k == Keys.P
+            reset!(game)
+        end
+        return
+    end
     if !Paused
         if (k == Keys.LEFT)
             send_input!(game, :move_left)
@@ -74,8 +130,6 @@ function on_key_down(g::Game, k)
             send_input!(game, :move_right)
         elseif (k == Keys.UP || k == Keys.X)
             send_input!(game, :rotate_clockwise)
-        elseif (k == Keys.LCTRL || k == Keys.Z)
-            # send_input!(game, :soft_drop)
         elseif (k == Keys.DOWN)
             send_input!(game, :rotate_counter_clockwise)
         elseif (k == Keys.SPACE)
@@ -95,48 +149,70 @@ function on_key_down(g::Game, k)
     end
 end
 
-function timeStep()
+"""
+Increments the timer.
+"""
+function tick()
     global Timer, game, Gravity
     Timer += 1
     if Timer >= Gravity
-        if play_step!(game)
-            reset!(game)
-        end
+        play_step!(game)
         levelUp()
         resetTimer()
     end
     return
 end
 
+"""
+Resets the timer to 0.
+"""
 function resetTimer()
     global Timer = 0
 end
 
-
-
+"""
+Base GameZero.jl function, called every frame. Draws everything on screen.
+"""
 function draw(g::Game)
     global game
     draw(bg)
-    drawBoard(game.grid)
-    txt = TextActor(string(Timer), "bold"; font_size = 50, color = Int[255, 255, 255, 255])
-    txt.pos = (0,0)
-    draw(txt)
-    lvl = TextActor(string(game.level), "bold"; font_size = 50, color = Int[255, 255, 255, 255])
-    lvl.center = (875,425)
-    draw(lvl)
-    scr = TextActor(string(game.score), "bold"; font_size = 50, color = Int[255, 255, 255, 255])
-    scr.center = (750,625)
-    draw(scr)
-    lnz = TextActor(string(game.line_count), "bold"; font_size = 50, color = Int[255, 255, 255, 255])
-    lnz.center = (750,825)
-    draw(lnz)
+    drawBoard()
+    drawNextPieces()
+    drawHeldPiece()
+
+    # to remove
+    # ticks = TextActor(string(Timer), "bold"; font_size = 50, color = Int[255, 255, 255, 255]) 
+    # ticks.pos = (0,0)
+    # draw(ticks)
+
+    # Draw stats
+    level = TextActor(string(game.level), "bold"; font_size = 50, color = Int[255, 255, 255, 255])
+    level.center = (875,425)
+    draw(level)
+    score = TextActor(string(game.score), "bold"; font_size = 50, color = Int[255, 255, 255, 255])
+    score.center = (750,625)
+    draw(score)
+    line_count = TextActor(string(game.line_count), "bold"; font_size = 50, color = Int[255, 255, 255, 255])
+    line_count.center = (750,825)
+    draw(line_count)
     if Paused
         draw(pause)
     end
+    if game.is_over
+        draw(gameover)
+    end
 end
 
+"""
+Base GameZero.jl function, called every frame. Updates the game state.
+"""
 function update(g::Game)
-    if !Paused
-        timeStep()
+    global game
+    if !Paused && !game.is_over
+        tick()
+        # Check for constant input for soft drop
+        if g.keyboard.Z || g.keyboard.LCTRL
+            tick()
+        end
     end
 end
